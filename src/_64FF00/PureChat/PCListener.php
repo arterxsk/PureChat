1ca22c26d19492248e168e701d0a433aa55499cd
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-03-15 21:02:32 +0100

    fix crash whith pureperm ?

--- src/_64FF00/PureChat/PCListener.php
+++ src/_64FF00/PureChat/PCListener.php
@@ -2,7 +2,7 @@
 
 namespace _64FF00\PureChat;
 
-use _64FF00\PurePerms\event\PPGroupChangedEvent;
+use _64FF00\PurePerms\EventManager\PPRankChangedEvent;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
@@ -10,66 +10,71 @@ use pocketmine\event\player\PlayerJoinEvent;
 
 use pocketmine\player\Player;
 
-class PCListener implements Listener {
-	/*
-		PureChat by 64FF00 (Twitter: @64FF00)
+class PCListener implements Listener
+{
+    /*
+        PureChat by 64FF00 (Twitter: @64FF00)
 
-		  888  888    .d8888b.      d8888  8888888888 8888888888 .d8888b.   .d8888b.
-		  888  888   d88P  Y88b    d8P888  888        888       d88P  Y88b d88P  Y88b
-		888888888888 888          d8P 888  888        888       888    888 888    888
-		  888  888   888d888b.   d8P  888  8888888    8888888   888    888 888    888
-		  888  888   888P "Y88b d88   888  888        888       888    888 888    888
-		888888888888 888    888 8888888888 888        888       888    888 888    888
-		  888  888   Y88b  d88P       888  888        888       Y88b  d88P Y88b  d88P
-		  888  888    "Y8888P"        888  888        888        "Y8888P"   "Y8888P"
-	*/
+          888  888    .d8888b.      d8888  8888888888 8888888888 .d8888b.   .d8888b.
+          888  888   d88P  Y88b    d8P888  888        888       d88P  Y88b d88P  Y88b
+        888888888888 888          d8P 888  888        888       888    888 888    888
+          888  888   888d888b.   d8P  888  8888888    8888888   888    888 888    888
+          888  888   888P "Y88b d88   888  888        888       888    888 888    888
+        888888888888 888    888 8888888888 888        888       888    888 888    888
+          888  888   Y88b  d88P       888  888        888       Y88b  d88P Y88b  d88P
+          888  888    "Y8888P"        888  888        888        "Y8888P"   "Y8888P"
+    */
 
-	private $plugin;
+    private $plugin;
 
-	public function __construct(PureChat $plugin) {
-		$this->plugin = $plugin;
-	}
+    public function __construct(PureChat $plugin)
+    {
+        $this->plugin = $plugin;
+    }
 
-	public function onGroupChanged(PPGroupChangedEvent $event) {
-		/** @var \pocketmine\player\IPlayer $player */
-		$player = $event->getPlayer();
+    public function onGroupChanged(PPRankChangedEvent $event)
+    {
+        /** @var \pocketmine\player\IPlayer $player */
+        $player = $event->getPlayer();
 
-		if ($player instanceof Player) {
-			$levelName = $this->plugin->getConfig()->get("enable-multiworld-chat") ? $player->getWorld()->getDisplayName() : null;
+        if ($player instanceof Player) {
+            $levelName = $this->plugin->getConfig()->get("enable-multiworld-chat") ? $player->getWorld()->getDisplayName() : null;
 
-			$nameTag = $this->plugin->getNametag($player, $levelName);
+            $nameTag = $this->plugin->getNametag($player, $levelName);
 
-			$player->setNameTag($nameTag);
-		}
-	}
+            $player->setNameTag($nameTag);
+        }
+    }
 
-	/**
-	 * @priority HIGH
-	 */
-	public function onPlayerJoin(PlayerJoinEvent $event) {
-		/** @var \pocketmine\player\Player $player */
-		$player = $event->getPlayer();
-		$levelName = $this->plugin->getConfig()->get("enable-multiworld-chat") ? $player->getWorld()->getDisplayName() : null;
+    /**
+     * @priority HIGH
+     */
+    public function onPlayerJoin(PlayerJoinEvent $event)
+    {
+        /** @var \pocketmine\player\Player $player */
+        $player = $event->getPlayer();
+        $levelName = $this->plugin->getConfig()->get("enable-multiworld-chat") ? $player->getWorld()->getDisplayName() : null;
 
-		$nameTag = $this->plugin->getNametag($player, $levelName);
+        $nameTag = $this->plugin->getNametag($player, $levelName);
 
-		$player->setNameTag($nameTag);
-	}
+        $player->setNameTag($nameTag);
+    }
 
-	/**
-	 * @priority HIGH
-	 */
-	public function onPlayerChat(PlayerChatEvent $event) {
-		if ($event->isCancelled()) {
-			return;
-		}
-		$player = $event->getPlayer();
-		$message = $event->getMessage();
+    /**
+     * @priority HIGH
+     */
+    public function onPlayerChat(PlayerChatEvent $event)
+    {
+        if ($event->isCancelled()) {
+            return;
+        }
+        $player = $event->getPlayer();
+        $message = $event->getMessage();
 
-		$levelName = $this->plugin->getConfig()->get("enable-multiworld-chat") ? $player->getWorld()->getDisplayName() : null;
+        $levelName = $this->plugin->getConfig()->get("enable-multiworld-chat") ? $player->getWorld()->getDisplayName() : null;
 
-		$chatFormat = $this->plugin->getChatFormat($player, $message, $levelName);
+        $chatFormat = $this->plugin->getChatFormat($player, $message, $levelName);
 
-		$event->setFormat($chatFormat);
-	}
+        $event->setFormat($chatFormat);
+    }
 }
